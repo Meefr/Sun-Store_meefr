a15dae953cb0a13b78a93e8c93147067c76425ac
mohamed essam <meefr25@gmail.com>
2022-08-23 15:45:39 +0200

    we

---
+++ watch/watch.js
@@ -0,0 +1,8 @@
+let img=document.querySelector('.im');
+let cont=document.querySelector('.cont');
+function phones(x){
+    img.src=x;
+}
+function colors(color){
+    cont.style.background=color;
+}
